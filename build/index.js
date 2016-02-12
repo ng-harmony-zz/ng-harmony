85069a8da799fa910a0e167747a30a5405d61c73
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2016-02-12 17:41:52 +0100

    Bump

--- build/index.js
+++ build/index.js
@@ -64,7 +64,7 @@ export class Harmony {
 }
 export class Controller extends Harmony {
     constructor (...args) {
-        super(args);
+        super(...args);
         for (let [key, fn] of this.iterate(this.constructor.prototype)) {
             if (typeof fn === "function" &&
                 key[0] === "$") {
